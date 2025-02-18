a930e83b6331bc78d3c24c46bd3fc77cdeaa253e
Emma <emraible@uw.edu>
2025-02-18 10:07:12 +0100

    added primereact and primeicons with theme working

--- maestri-app/src/App.tsx
+++ maestri-app/src/App.tsx
@@ -1,9 +1,17 @@
 
-function App() {
+import 'primereact/resources/themes/lara-dark-amber/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
+import { Button } from "primereact/button";
+
+function App() {
   return (
     <div>
       Maestri App, coming soon
+      <div>
+        <Button>Test Button</Button>
+      </div>
     </div>
   )
 }
