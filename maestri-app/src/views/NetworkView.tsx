e9b1ad9045c67895c5fcec93a6b988856031b6e1
Isak Larsson <isak.valter.larsson@gmail.com>
2025-02-26 11:41:54 +0100

    Add structure for artist images

--- maestri-app/src/views/NetworkView.tsx
+++ maestri-app/src/views/NetworkView.tsx
@@ -1,8 +1,10 @@
 import { Button } from "primereact/button";
 import { ResponsiveNetwork } from '@nivo/network'
 import NetworkData from '../../../data/network.json'  
+import ArtistImages from '../../../data/images.json'  
 import { useState } from "react";
 import { Card } from 'primereact/card';
+import {animated, to} from "@react-spring/web";
 
 function Network() {
   // make sure parent container have a defined height when using
@@ -54,6 +56,7 @@ const max_local_collaborations = biggest_local_collaborator.num_collaborations;
         onClick={(node: NetworkComputedNode, event: MouseEvent)=>{
           setArtistId(node.id)
         }}
+        nodeComponent={n=>nodeComponent(n)}
         nodeTooltip={(node)=>{
           let name = node.node.data.name
           if (name == ""){
@@ -89,5 +92,80 @@ function edgeSize(node, max_local_collaborations){
     const min_size = 2;
     return Math.floor(normalized_collaborations*(max_size-min_size) + min_size);
 }
+
+function edgeDistance(edge, max_local_collaborations){
+  
+  const main_artist_id = edge.source
+  const collaborator_artist_id = edge.target
+  const nodes: Array<Object> = NetworkData[main_artist_id]["nodes"]
+
+  const collaborations = nodes.find(n=>(n.id == collaborator_artist_id)).num_collaborations
+
+  var normalized_collaborations = (collaborations) / max_local_collaborations;
+  normalized_collaborations = 1 - normalized_collaborations; // Inverse relationship. More collaborations = closer to each other
+  console.log(normalized_collaborations)
+  const max_size = 150;
+  const min_size = 75;
+  return Math.floor(normalized_collaborations*(max_size-min_size) + min_size);
+}
+
+function nodeComponent(nodeIn){
+  const  {
+    node,
+    animated: animatedProps,
+    onClick,
+    onMouseEnter,
+    onMouseMove,
+    onMouseLeave,
+  } = nodeIn
+
+  var artistImageUrl = "https://images.genius.com/073372f6cd316f7c68b4c4b7d8c610c9.675x675x1.jpg"
+  // TODO: Actually use the images when we have the genius ids for the artists
+  // if (typeof node.id == 'undefined'){
+  //   console.log(node)
+  // } else {
+  //   artistImageUrl = ArtistImages[node.id]["imageURL"]
+  // }
+
+  
+  return <>
+    <defs>
+      <clipPath id={`circle-clip-${node.id}`} clipPathUnits="objectBoundingBox">
+        <circle cx="0.5" cy="0.5" r="0.5" />
+      </clipPath>
+    </defs>
+
+    <animated.g
+      transform={to(
+        [animatedProps.x, animatedProps.y, animatedProps.scale],
+        (x, y, scale) => `translate(${x},${y}) scale(${scale})`
+      )}
+    >
+      <animated.image
+        href={artistImageUrl}
+        width={animatedProps.size}
+        height={animatedProps.size}
+        x={to(animatedProps.size, size => -size / 2)}
+        y={to(animatedProps.size, size => -size / 2)}
+        clipPath={`url(#circle-clip-${node.id})`}
+        preserveAspectRatio="xMidYMid slice"
+        opacity={animatedProps.opacity}
+      />
+
+      <animated.circle
+        r={to([animatedProps.size], size => size / 2)}
+        strokeWidth={animatedProps.borderWidth}
+        stroke={animatedProps.borderColor}
+        fill="transparent"
+        onClick={onClick ? event => onClick(node, event) : undefined}
+        onMouseEnter={onMouseEnter ? event => onMouseEnter(node, event) : undefined}
+        onMouseMove={onMouseMove ? event => onMouseMove(node, event) : undefined}
+        onMouseLeave={onMouseLeave ? event => onMouseLeave(node, event) : undefined}
+        data-testid={`node.${node.id}`}
+      />
+    </animated.g>
+  </>
+}
+
 
 export default Network
